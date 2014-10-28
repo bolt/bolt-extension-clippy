889ec98eeae6d37d2ffac727627c290870d69657
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-28 13:29:33 +0000

    Start with basic extension class file
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

---
+++ Extension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bolt\Extension\Gawain\Clippy;
+
+use Bolt;
+
+class Extension extends \Bolt\BaseExtension
+{
+
+    public function getName()
+    {
+        return "Clippy";
+    }
+
+    public function initialize()
+    {
+    }
+}
