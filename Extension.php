56f7bc5d0390796b8cb671590c8fb18a806182ec
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-30 12:28:25 +0000

    Make agent optional
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- Extension.php
+++ Extension.php
@@ -26,8 +26,27 @@ class Extension extends \Bolt\BaseExtension
         $this->addCss('lib/clippy.js/build/clippy.css');
         $this->addJavascript('lib/clippy.js/build/clippy.min.js', true);
 
+        // Add path
         $this->app['twig.loader.filesystem']->addPath(__DIR__ . "/assets");
-        $html = $this->app['render']->render('clippy.twig', array());
+
+        // Render the JS
+        $html = $this->app['render']->render('clippy.twig', array(
+            'agent' => $this->config['agent']
+        ));
+
+        // Add the snippets
         $this->addSnippet(SnippetLocation::END_OF_HTML, $html);
+    }
+
+    /**
+     * Default config options
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return array(
+            'agent' => 'Clippy'
+        );
     }
 }
