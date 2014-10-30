a37d468029f7a63198f31b1ca5af0b1c59096c0a
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-30 12:08:23 +0000

    Insert Clippy
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- Extension.php
+++ Extension.php
@@ -2,7 +2,8 @@
 
 namespace Bolt\Extension\Gawain\Clippy;
 
-use Bolt;
+use Bolt\Application;
+use Bolt\Extensions\Snippets\Location as SnippetLocation;
 
 class Extension extends \Bolt\BaseExtension
 {
@@ -12,7 +13,21 @@ class Extension extends \Bolt\BaseExtension
         return "Clippy";
     }
 
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+        if ($this->app['config']->getWhichEnd()=='backend') {
+            $this->app['htmlsnippets'] = true;
+        }
+    }
+
     public function initialize()
     {
+        $this->addCss('lib/clippy.js/build/clippy.css');
+        $this->addJavascript('lib/clippy.js/build/clippy.min.js', true);
+
+        $this->app['twig.loader.filesystem']->addPath(__DIR__ . "/assets");
+        $html = $this->app['render']->render('clippy.twig', array());
+        $this->addSnippet(SnippetLocation::END_OF_HTML, $html);
     }
 }
