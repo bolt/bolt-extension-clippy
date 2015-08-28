1483923ac8627f3c600e8f1047e67af1845a3157
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-28 11:53:52 +0100

    Code clean up

--- Extension.php
+++ Extension.php
@@ -5,44 +5,36 @@ namespace Bolt\Extension\Gawain\Clippy;
 use Bolt\Application;
 use Bolt\BaseExtension;
 use Bolt\Extensions\Snippets\Location as SnippetLocation;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class Extension extends BaseExtension
 {
-
     public function getName()
     {
-        return "Clippy";
+        return 'Clippy';
     }
 
     public function __construct(Application $app)
     {
         parent::__construct($app);
-        if ($this->app['config']->getWhichEnd() == 'backend') {
+        if ($this->app['config']->getWhichEnd() === 'backend') {
             $this->app['htmlsnippets'] = true;
         }
     }
 
     public function initialize()
     {
-        if ($this->app['config']->getWhichEnd() == 'backend') {
+        if ($this->app['config']->getWhichEnd() === 'backend') {
             $this->addCss('lib/clippy.js/clippy.css');
             $this->addJavascript('lib/clippy.js/clippy.min.js', true);
 
             // Add path
-            $this->app['twig.loader.filesystem']->addPath(__DIR__ . "/assets");
+            $this->app['twig.loader.filesystem']->addPath(__DIR__ . '/assets');
 
             // Render the JS
             $html = $this->app['render']->render('clippy.twig', array(
-                'agent' => $this->config['agent'],
+                'agent'    => $this->config['agent'],
                 'messages' => $this->app['session']->getFlashBag()->peek('error')
             ));
-
-            // Enable snippets to be loaded in the backend
-            $this->app->before(function (Request $request, Application $app) {
-                $request->attributes->set('allow_snippets', true);
-            });
 
             // Add the snippets
             $this->addSnippet(SnippetLocation::END_OF_HTML, $html);
